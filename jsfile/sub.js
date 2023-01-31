a2d22bee39c0d30b69406b96b371183279698726
NamSeongJu0 <119376125+NamSeongJu0@users.noreply.github.com>
2023-02-01 00:21:32 +0900

    02/01
    벌써 2월이네 ㄷㄷ

--- jsfile/sub.js
+++ jsfile/sub.js
@@ -1,71 +1,71 @@
 // header active 효과
-$(function() {
-    $(window).resize(function(){
-        if(window.innerWidth > 1199){
-           $('.spMenu_btn').hide();
-           $('.nav, .nav_R .navL').show();
-           $("#side-nav").hide();
+$(function () {
+    $(window).resize(function () {
+        if (window.innerWidth > 1199) {
+            $('.spMenu_btn').hide();
+            $('.nav, .nav_R .navL').show();
+            $("#side-nav").hide();
         }
-        else{
+        else {
             $('.spMenu_btn').show();
             $('.nav, .nav_R .navL').hide();
         }
-       });
+    });
 });
 
 
 // 이미지 전환
 $(function () {
-    function prev(){
+    function prev() {
 
         $('#slidewrap .slide li:last').prependTo('#slidewrap .slide');
-        
+
         $('#slidewrap .slide').css('margin-left', 1100);
-        
-        $('#slidewrap .slide').stop().animate({marginLeft: 0},800);
-        
-        }
 
-        function next(){
-        
-        $('#slidewrap .slide').stop().animate({marginLeft: -1100},800, function(){
-        
-        $('#slidewrap .slide li:first').appendTo('#slidewrap .slide');
-        
-        $('#slidewrap .slide').css({marginLeft: 0});
-        
+        $('#slidewrap .slide').stop().animate({ marginLeft: 0 }, 800);
+
+    }
+
+    function next() {
+
+        $('#slidewrap .slide').stop().animate({ marginLeft: -1100 }, 800, function () {
+
+            $('#slidewrap .slide li:first').appendTo('#slidewrap .slide');
+
+            $('#slidewrap .slide').css({ marginLeft: 0 });
+
         });
-        
-        }
 
-        setInterval(next, 3000);
+    }
 
-        
-        //버튼 슬라이드
-        
-        $('.prev').click(function(){
-        
+    setInterval(next, 3000);
+
+
+    //버튼 슬라이드
+
+    $('.prev').click(function () {
+
         prev();
-        
-        });
 
-        
-        $('.next').click(function(){
-        
+    });
+
+
+    $('.next').click(function () {
+
         next();
-        
-        });
+
+    });
 });
 
 
 // product, activities 슬라이드 효과
 
-$(function() {
+$(function () {
     $('.navL_M > li:first-child > .navL_S, .navL_M > li:nth-of-type(2) > .navL_S').hide();
 
-    $('.navL_M > li:first-child, .navL_M > li:nth-of-type(2)').hover(function(){
+    $('.navL_M > li:first-child, .navL_M > li:nth-of-type(2)').hover(function () {
         $(this).children('.navL_S').stop().fadeIn(200);
-    }, function(){
+    }, function () {
         $(this).children('.navL_S').stop().fadeOut(200);
     });
 });
@@ -77,31 +77,31 @@ const contentEm = document.querySelectorAll("header");
 // const infoLi = document.querySelectorAll(".info.bottom li");
 
 // 움직임 효과
-$(function() {
-    $('header').mousemove(function(e){
-        gsap.to(".cursor", {duration: 0.4, left: e.pageX, top: e.pageY});
+$(function () {
+    $('header').mousemove(function (e) {
+        gsap.to(".cursor", { duration: 0.4, left: e.pageX, top: e.pageY });
     });
 
-    $('header').mouseover(function(e){
+    $('header').mouseover(function (e) {
         $('.cursor').css("display", "block");
     });
-    $('header').mouseout(function(e){
+    $('header').mouseout(function (e) {
         $('.cursor').css("display", "none");
     });
-    
+
 
     // 오버 효과
-    $(".nav .navL .navL_M li, header .logo_1, .sns a, header .spMenu_btn ul").hover(function(){
+    $(".nav .navL .navL_M li, header .logo_1, .sns a, header .spMenu_btn ul").hover(function () {
         $(".cursor").addClass("active");
-    }, function(){
+    }, function () {
         $(".cursor").removeClass("active");
     });
-    
+
     // 제목에 마우스 오버 했을 때
-    $(".sns ul li").hover(function(){
-        $(this).children().css({'color': 'silver', 'transition': 'all 0.5s ease-in'});
-    }, function(){
-        $(this).children().css({'color': 'white', 'transition': 'all 0.5s ease-in'});
+    $(".sns ul li").hover(function () {
+        $(this).children().css({ 'color': 'silver', 'transition': 'all 0.5s ease-in' });
+    }, function () {
+        $(this).children().css({ 'color': 'white', 'transition': 'all 0.5s ease-in' });
     });
 });
 
@@ -130,13 +130,31 @@ $(function () {
     });
 
 });
-  
 
-    // 메뉴바
-    $(function() {
-        $('#side-nav > ul:first-child li:first-child > .navT_S, #side-nav > ul:first-child li:nth-of-type(2) > .navT_S').hide();
-    
-        $('#side-nav > ul:first-child li:first-child, #side-nav > ul:first-child li:nth-of-type(2)').click(function(){
-            $(this).children('.navT_S').stop().slideToggle(800);
-        });
+
+// 메뉴바
+$(function () {
+    $('#side-nav > ul:first-child li:first-child > .navT_S, #side-nav > ul:first-child li:nth-of-type(2) > .navT_S').hide();
+
+    $('#side-nav > ul:first-child li:first-child, #side-nav > ul:first-child li:nth-of-type(2)').click(function () {
+        $(this).children('.navT_S').stop().slideToggle(800);
     });
+});
+
+
+// top버튼
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 700) {
+            $('.topbtn').stop().fadeIn(500);
+        } else {
+            $('.topbtn').stop().fadeOut(500);
+        }
+    });
+
+// 탑 버튼 속도 조절
+    $('.topbtn').click(function (event) {
+        event.preventDefault();
+        $('html, body').animate({ scrollTop: 0 }, 1800);
+    });
+});
